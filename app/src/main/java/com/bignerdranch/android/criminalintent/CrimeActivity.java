67fc45646eb065f94fb0ef5d910eb8951a73871c
michaeljosephpurdy <michaeljosephpurdy@gmail.com>
2018-12-03 22:37:54 -0500

    Renamed layout to be more generic

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -7,16 +7,15 @@ import android.os.Bundle;
 
 public class CrimeActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
+        setContentView(R.layout.activity_fragment);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
-
         Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
         if (fragment == null)
         {
             fragment = new CrimeFragment();
